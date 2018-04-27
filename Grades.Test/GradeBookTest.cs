9cfaaf7444d54c145d920b434ef0aef1281b3fbd
Hiten1928 <hg1928@rit.edu>
2018-04-27 00:00:07 -0400

    Adding Test Cases

--- Grades.Test/GradeBookTest.cs
+++ Grades.Test/GradeBookTest.cs
@@ -4,11 +4,26 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Grades.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class GradeBookTest
+    
     {
         [TestMethod]
         public void TestMethod1()
         {
+
+            GradeBook book_test = new GradeBook();
+
+            book_test.AddGrade(10);
+            book_test.AddGrade(80);
+
+            GradeStatistics stats_test = new GradeStatistics();
+
+            stats_test = book_test.ComputeStatistics();
+
+            Assert.Equals(80, stats_test.Maximum);
+            Assert.Equals(10, stats_test.Minimum);
+            Assert.Equals(45, stats_test.Average);
+
         }
     }
 }
