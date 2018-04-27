d1b4ae205d1a111606130187f50eff82b4a65abe
Hiten1928 <hg1928@rit.edu>
2018-04-27 00:21:03 -0400

    Added delta tests

--- Grades.Test/GradeBookTest.cs
+++ Grades.Test/GradeBookTest.cs
@@ -4,13 +4,30 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Grades.Test
 {
     [TestClass]
-    public class GradeBookTest
+     public class GradeBookTest
     
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAverage()
         {
 
+            GradeBook book_test = new GradeBook();
+
+            book_test.AddGrade(82);
+            book_test.AddGrade(123);
+
+            GradeStatistics stats_test = new GradeStatistics();
+
+            stats_test = book_test.ComputeStatistics();
+
+            
+            Assert.AreEqual(102.5, stats_test.Average, .01);
+
+        }
+
+        [TestMethod]
+        public void TestHighest()
+        {
             GradeBook book_test = new GradeBook();
 
             book_test.AddGrade(10);
@@ -20,10 +37,23 @@ namespace Grades.Test
 
             stats_test = book_test.ComputeStatistics();
 
-            Assert.Equals(80, stats_test.Maximum);
-            Assert.Equals(10, stats_test.Minimum);
-            Assert.Equals(45, stats_test.Average);
+            Assert.AreEqual(80, stats_test.Maximum);
+            
+        }
 
+        [TestMethod]
+        public void TestLowest()
+        {
+            GradeBook book_test = new GradeBook();
+
+            book_test.AddGrade(10);
+            book_test.AddGrade(80);
+
+            GradeStatistics stats_test = new GradeStatistics();
+
+            stats_test = book_test.ComputeStatistics();
+
+            Assert.AreEqual(10, stats_test.Minimum);
         }
     }
 }
